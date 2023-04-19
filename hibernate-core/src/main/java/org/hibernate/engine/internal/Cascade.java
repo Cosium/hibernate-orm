337dfd165729fd3fbc5e7e83ba4516b1117c2f14
Grégoire Rouillon <grouillon@cosium.com>
2023-04-19 14:11:12 +0200

    Replace 'AtomicInteger' by a 'final int' on cascading action

--- hibernate-core/src/main/java/org/hibernate/engine/internal/Cascade.java
+++ hibernate-core/src/main/java/org/hibernate/engine/internal/Cascade.java
@@ -11,7 +11,6 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 import org.hibernate.HibernateException;
 import org.hibernate.bytecode.enhance.spi.interceptor.LazyAttributeLoadingInterceptor;
@@ -178,13 +177,13 @@ public final class Cascade {
 					}
 					// If the property is uninitialized, then there cannot be any orphans.
 					if ( action.deleteOrphans() && !isUninitializedProperty ) {
-						AtomicInteger index = new AtomicInteger(i);
+						final int index = i;
 						cascadeLogicalOneToOneOrphanRemoval(
 								action,
 								eventSource,
 								null,
 								parent,
-								() -> persister.getValue( parent, index.get() ),
+								() -> persister.getValue( parent, index ),
 								type,
 								style,
 								propertyName,
